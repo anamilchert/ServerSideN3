5dade3a8a6600562545082f9e14c63a025d7f3bb
anamilchert <ana.milchert@hotmail.com>
2024-06-26 14:23:41 -0300

    criando um novo prestador e pegando todos os prestadores

---
+++ Controllers/prestadorController.js
@@ -0,0 +1,21 @@
+const Prestador = require('../Models/prestador');
+
+exports.createPrestador = async (req, res) => {
+    try {
+        const { nome_prestador, tempo_experiencia, categoriaId } = req.body;
+        const prestador = new Prestador({ nome_prestador, tempo_experiencia, categoriaId });
+        await prestador.save();
+        res.status(201).json(prestador);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao criar prestador' });
+    }
+};
+
+exports.getPrestadores = async (res) => {
+    try {
+        const prestadores = await Prestador.find().populate('categoriaId');
+        res.json(prestadores);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar prestadores' });
+    }
+};
