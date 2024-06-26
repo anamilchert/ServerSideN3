a8c55b6c6d6175f50659cb372259ca6f1b30ed56
anamilchert <ana.milchert@hotmail.com>
2024-06-26 17:10:20 -0300

    correção da rota

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const connectDB = require('./config/database');
 
 app.use(bodyParser.json());
 
-const categoriaRoutes = require('./Routes/categoriaRoutes');
-const prestadorRoutes = require('./Routes/prestadorRoutes');
-const servicoRoutes = require('./Routes/servicoRoutes');
+const categoriaRoutes = require('/Routes/categoriaRoutes');
+const prestadorRoutes = require('/Routes/prestadorRoutes');
+const servicoRoutes = require('/Routes/servicoRoutes');
 
 app.use('/api/categorias', categoriaRoutes);
 app.use('/api/prestadores', prestadorRoutes);
